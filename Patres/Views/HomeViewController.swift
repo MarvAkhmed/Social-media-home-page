a19e615c9398c79b321294a4f6ca643544a2daba
Marwa <MarvAkhmed@stud.kpfu.ru>
2025-03-17 21:13:41 +0300

    removed main story board and created the folders for mvvm

---
+++ Patres/Views/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Patres
+//
+//  Created by Marwa Awad on 17.03.2025.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
